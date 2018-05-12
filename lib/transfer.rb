abb136f613b55edb15644615db0a2816dacfd6dd
Kitty Davies <kittyfdavies@gmail.com>
2018-05-12 20:51:56 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
 
   def execute_transaction
-    while @status == "pending"
+    if @status == "pending"
       sender.balance = sender.balance - @amount
       receiver.balance = receiver.balance + @amount
       if !receiver.valid? || !sender.valid?
